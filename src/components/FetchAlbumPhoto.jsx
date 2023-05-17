d7e08e50b27e6a33e31727247e17e8988535995f
Dmytro Krakhmalov <113544591+strigoi229@users.noreply.github.com>
2023-05-17 13:06:25 +0300

    Delete FetchAlbumPhoto.jsx

--- src/components/FetchAlbumPhoto.jsx
+++
@@ -1,6 +0,0 @@
-export  function FetchAlbumPhoto({params}) {
-    const fetchPhoto = fetch (`https://jsonplaceholder.typicode.com/photos?albumId=${params.albumId}`)
-    .then(response => response.json())
-    .then(result => result);
-    return fetchPhoto;
-}
