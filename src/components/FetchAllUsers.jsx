8f8fc82b2779ee49112d85673a5e63d7a17e9b58
Dmytro Krakhmalov <113544591+strigoi229@users.noreply.github.com>
2023-05-17 13:06:35 +0300

    Delete FetchAllUsers.jsx

--- src/components/FetchAllUsers.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export  function FetchAllUsers() {
-    const fetchUsers = fetch ("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(result => result);
-
-    return fetchUsers;
-}
