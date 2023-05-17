24a482cb2a41e2d4070f28d103d4dbde8bd3f8fd
Dmytro Krakhmalov <113544591+strigoi229@users.noreply.github.com>
2023-05-17 13:06:14 +0300

    Delete FetchUserAlbums.jsx

--- src/components/FetchUserAlbums.jsx
+++
@@ -1,8 +0,0 @@
-
-
-export function FetchUserAlbums({params}) {
-    const fetchAlbums = fetch (`https://jsonplaceholder.typicode.com/albums?userId=${params.userId}`)
-    .then(response => response.json())
-    .then(result => result);
-    return fetchAlbums;
-}
