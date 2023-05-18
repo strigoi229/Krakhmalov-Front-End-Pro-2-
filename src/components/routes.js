4449f7ad9aa411687f01405a29a656ec5f681006
Dmytro Krakhmalov <113544591+strigoi229@users.noreply.github.com>
2023-05-18 12:30:14 +0300

    Delete src directory

--- src/components/routes.js
+++
@@ -1,5 +0,0 @@
-export const ROUTES = {
-    users: "/",
-    albums: (userId) => (userId ? `/albums/${userId}` : "/albums/:userId"), 
-    photos: (albumId) => (albumId ? `/albums/photos/${albumId}` : "/albums/photos/:albumId")
-  }
